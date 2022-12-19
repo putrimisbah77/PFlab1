2268639103d962ea613294f6d709d54b1bdcbdd4
pakistan <Misbahh77777@gmail.com>
2022-12-18 23:17:23 -0800

    pd week1

---
+++ text01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+main() 
+{
+cout <<"***************************************";
+cout <<endl;
+cout <<"*                                     *";
+cout <<endl;
+cout <<"*        ----My name Misbah----       *";
+cout <<endl;
+cout <<"*    ---I am a graduated student---   *";
+cout <<endl;
+cout <<"*                                     *";
+cout <<endl;
+cout <<"*                                     *";
+cout <<endl;
+cout <<"*                                     *";
+cout <<endl;
+cout <<"***************************************";
+
+}
